a47c8e4671e7863ac17fb4a5eeebb23da6a245a3
Erica Yee <eeyusongen@gmail.com>
2025-05-24 13:31:43 -0700

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    let money = $state(0);
+
+    function increaseMoney() {
+        money += 1;
+    }
+</script>
+
+<main>
+    <h1>Scrappy manages the B's</h1>
+
+    <div class="stats-container">
+        <div>Energy: </div>
+        <div>Money: {money}</div>
+        <div>Community trust: </div>
+        <div>Influence: </div>
+    </div>
+
+    <div class="buttons-conatiner">
+        <button onclick={increaseMoney}>Click for money</button>
+    </div>
+</main>
+
+<style>
+    main {
+
+    }
+</style>
